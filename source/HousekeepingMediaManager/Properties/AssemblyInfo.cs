ab2189050e864a5a816b16a2764bba0d99dc81ed
Adriano Fabri <afabri@gmail.com>
2015-02-04 11:28:23 +0100

    Fixed: Issues #1 and #3

--- source/HousekeepingMediaManager/Properties/AssemblyInfo.cs
+++ source/HousekeepingMediaManager/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("F.A.L.M. Housekeeping - AppTree")]
+[assembly: AssemblyTitle("F.A.L.M. Housekeeping - Media Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
